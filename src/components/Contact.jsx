ac880a171bb604d601aa00dda50853c797cb3cda
DevAbdelrady <ahmedmc95@gmail.com>
2024-02-10 01:26:33 +0200

    version 1.0

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { motion } from "framer-motion"
 import { fadeIn } from '../variants'
+import emailjs from '@emailjs/browser';
 const Contact = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        const templateParams = {
+            from_name: name,
+            from_email: email,
+            message: message,
+        };
+        const serviceId = 'service_50tbj9v'
+        const templateId = 'template_enuavnj'
+        const publicKey = 'cYZg3Xcwfc0S0b0OF'
+
+        emailjs
+            .send(serviceId, templateId, templateParams, publicKey)
+            .then(
+                (res) => {
+                    console.log('SUCCESS!', res);
+                    setName('');
+                    sendEmail('');
+                    setMessage('');
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+            );
+    }
     return (
         <section id='contact' className='section mt-[500px]'>
             <div className='container mx-auto'>
@@ -29,10 +60,34 @@ const Contact = () => {
                         viewport={{ once: false, amount: 0.3 }}
                         className='flex-1 lg:mt-0 mt-8 border rounded-2xl flex flex-col gap-y-5 pb-24 p-6 items-start '
                     >
-                        <input type='text' className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-sky-500 transition-all duration-300' placeholder='Enter a name' />
-                        <input type='email' className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-sky-500 transition-all duration-300' placeholder='Enter a Email' />
-                        <textarea cols="30" rows="5" className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-sky-500 transition-all duration-300 resize-none' placeholder='Enter a message'></textarea>
-                        <button className='btn btn-lg'> Send message</button>
+                        <form onSubmit={(e) => sendEmail(e)}>
+                            <input
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                                type='text'
+                                name="user_name"
+                                className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-sky-500 transition-all duration-300'
+                                placeholder='Enter a name'
+                            />
+                            <input
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                type='email'
+                                name="user_email"
+                                className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-sky-500 transition-all duration-300'
+                                placeholder='Enter an Email'
+                            />
+                            <textarea
+                                value={message}
+                                onChange={(e) => setMessage(e.target.value)}
+                                name="message"
+                                cols="30"
+                                rows="5"
+                                className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-sky-500 transition-all duration-300 resize-none'
+                                placeholder='Enter a message'
+                            ></textarea>
+                            <button type="submit" className='btn btn-lg'> Send message</button>
+                        </form>
                     </motion.div>
                 </div>
             </div>
